c820bf9ade344bb2e047e995fdf5e148e8f262f6
“zannew“ <wonny21292@gmail.com>
2022-03-20 21:38:21 +0900

    EXERCISE : 싱글 링크드 리스트 조회, 삭제 예제(2)

--- src/linkedList/SingleLinkedList.java
+++ src/linkedList/SingleLinkedList.java
@@ -19,7 +19,7 @@ public class SingleLinkedList<T> {
         } else {
             Node<T> node = this.head;
             while(node.next != null){
-                node= node.next;
+                node = node.next;
             }
             node.next = new Node<T>(data);
         }
@@ -36,17 +36,78 @@ public class SingleLinkedList<T> {
         }
     }
 
+    public void addNodeInside(T data, T isData){
+        Node<T> searchedNode = this.search(isData);
+
+        if (searchedNode == null) {
+           this.addNode(data);
+        } else {
+            Node<T> nextNode = searchedNode.next;
+            // 1. 기존 node의 next가 새로운 node를 가리키게 함
+            searchedNode.next = new Node<>(data);
+            // 2. 새로 넣어준 node의 next가 기존 node가 가리키던 next를 가리키게 함
+            searchedNode.next.next = nextNode;
+        }
+    }
+
+    private Node<T> search(T data) {
+        if(this.head == null){
+            return null;
+        } else {
+            Node<T> node = this.head;
+            while (node != null){
+                if (node.data == data){
+                    return node;
+                } else {
+                    node = node.next;
+                }
+            }
+            return null;
+        }
+    }
+
+    private boolean deleteNode(T isData){
+        if(this.head == null){
+            return false;
+        } else {
+            Node<T> node = this.head;
+            // 삭제하려는 data가 head 인 경우
+            if (node.data == isData){
+                this.head = this.head.next;
+                return true;
+            } else {
+                while(node.next != null){
+                    if(node.next.data == isData){
+                        node.next = node.next.next;
+                        return true;
+                    }
+                    node = node.next;
+                }
+                return false;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         SingleLinkedList<Integer> myLinkedList = new SingleLinkedList<>();
         myLinkedList.addNode(1);
-        System.out.println(myLinkedList.head.data);
         myLinkedList.addNode(2);
-        System.out.println(myLinkedList.head.data);
-        // head 의 next 의 data
-        System.out.println(myLinkedList.head.next.data);
-
         myLinkedList.addNode(3);
+        myLinkedList.addNode(4);
+        myLinkedList.addNode(5);
 
+        myLinkedList.addNodeInside(5, 1);
+        myLinkedList.addNodeInside(6, 3);
+        myLinkedList.addNodeInside(7, 20);
+
+        // 헤드 삭제
+        myLinkedList.deleteNode(1);
+        // 중간 삭제
+        myLinkedList.deleteNode(3);
+        // 끝 삭제
+        myLinkedList.deleteNode(5);
         myLinkedList.printAll();
+
+
     }
 }
